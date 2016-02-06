a7a5401a77f240c27a584d8335323292c4c171e6
David Gamba <davidgamba@gmail.com>
2016-02-06 00:26:05 -0700

    Add ymlutils

---
+++ ymlutils/ymlutils.go
@@ -0,0 +1,62 @@
+package ymlutils
+
+import (
+	"fmt"
+	"github.com/davidgamba/go-utils/utils"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"strings"
+)
+
+// YML object
+type YML struct {
+	Filename string
+	data     []byte
+	Tree     interface{}
+}
+
+// New returns a pointer to a YML object
+func New(filename string) (*YML, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	var tree interface{}
+	err = yaml.Unmarshal(data, &tree)
+	if err != nil {
+		return nil, err
+	}
+	return &YML{Filename: filename, data: data, Tree: tree}, nil
+}
+
+// NavigateTree passthrough to utils.NavigateTree
+func NavigateTree(i interface{}, path []string) (interface{}, bool) {
+	return utils.NavigateTree(i, path)
+}
+
+// YMLQuery returns the yml object designated by path.
+// Example:
+// YMLQuery("level1/level2/level3")
+func (y *YML) YMLQuery(path string) (interface{}, bool) {
+	current, ok := utils.NavigateTree(y.Tree, strings.Split(path, "/"))
+	return current, ok
+}
+
+// YMLGetString returns a string designated by path.
+func (y *YML) YMLGetString(path string) (string, error) {
+	i, ok := y.YMLQuery(path)
+	if ok {
+		switch i.(type) {
+		case string:
+			return i.(string), nil
+		default:
+			return "", fmt.Errorf("yaml path didn't return a valid string")
+		}
+	}
+	return "", fmt.Errorf("yaml path not found")
+}
+
+// Unmarshal will unmarshal to the given structure.
+func (y *YML) Unmarshal(out interface{}) error {
+	return yaml.Unmarshal(y.data, out)
+}
