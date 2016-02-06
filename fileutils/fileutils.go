d43e1eb79bbd83272cdce32845c7859dc7fbac21
David Gamba <davidgamba@gmail.com>
2016-02-05 22:31:23 -0700

    Add fileutils.CopyFile

---
+++ fileutils/fileutils.go
@@ -0,0 +1,35 @@
+package fileutils
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+// CopyFile copies the contents of the file named src to the file named
+// by dst. The file will be created if it does not already exist. If the
+// destination file exists, all it's contents will be replaced by the contents
+// of the source file.
+func CopyFile(src, dst string) error {
+	fmt.Printf("Copy: %s %s\n", src, dst)
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		cerr := out.Close()
+		if err == nil {
+			err = cerr
+		}
+	}()
+	if _, err = io.Copy(out, in); err != nil {
+		return err
+	}
+	err = out.Sync()
+	return err
+}
